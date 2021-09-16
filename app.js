4eace5e7afc99ac5d7c2b15b6fe430906c3bc624
Akash Shrimali <31008778+shrimaliakash@users.noreply.github.com>
2021-09-17 00:36:17 +0530

    level2 add

--- app.js
+++ app.js
@@ -188,13 +188,215 @@ document.addEventListener('DOMContentLoaded', () => {
 		resultDisplay.textContent = cardsWon.length;
 		if(cardsWon.length === cardArray.length/2) {
 			resultDisplay.textContent = cardsWon.length;
-			winnerDisplay.textContent = 'Congratulations! You them all!';
+			//winnerDisplay.textContent = 'Congratulations! You them all!';
 			// var head = document.getElementsByTagName('head')[0];
 			// var newScript = document.createElement('script');
 			// newScript.src = 'level2.js';
 			// newScript.charset = 'text/javascript';
 			// head.appendChild(newScript);
-			location.reload();
+			//location.reload();
+			
+			const cardArray1 = [
+		{
+			name:'dog',
+			img: 'dog.jpg'
+		},
+		{
+			name:'elephant',
+			img: 'elephant.jpg'
+		},
+		{
+			name:'fox',
+			img: 'fox.jpg'
+		},
+		{
+			name:'monkey',
+			img: 'monkey.jpg'
+		},
+		{
+			name:'snake',
+			img: 'snake.jpg'
+		},
+		{
+			name:'tiger',
+			img: 'tiger.jpg'
+		},
+		{
+			name:'dog',
+			img: 'dog.jpg'
+		},
+		{
+			name:'monkey',
+			img: 'monkey.jpg'
+		},
+		{
+			name:'fox',
+			img: 'fox.jpg'
+		},
+		{
+			name:'snake',
+			img: 'snake.jpg'
+		},
+		{
+			name:'tiger',
+			img: 'tiger.jpg'
+		},
+		{
+			name:'elephant',
+			img: 'elephant.jpg'
+		},
+		{
+			name:'dog',
+			img: 'dog.jpg'
+		},
+		{
+			name:'elephant',
+			img: 'elephant.jpg'
+		},
+		{
+			name:'fox',
+			img: 'fox.jpg'
+		},
+		{
+			name:'monkey',
+			img: 'monkey.jpg'
+		},
+		{
+			name:'snake',
+			img: 'snake.jpg'
+		},
+		{
+			name:'tiger',
+			img: 'tiger.jpg'
+		},
+		{
+			name:'dog',
+			img: 'dog.jpg'
+		},
+		{
+			name:'monkey',
+			img: 'monkey.jpg'
+		},
+		{
+			name:'fox',
+			img: 'fox.jpg'
+		},
+		{
+			name:'snake',
+			img: 'snake.jpg'
+		},
+		{
+			name:'tiger',
+			img: 'tiger.jpg'
+		},
+		{
+			name:'elephant',
+			img: 'elephant.jpg'
+		},
+		{
+			name:'dog',
+			img: 'dog.jpg'
+		},
+		{
+			name:'elephant',
+			img: 'elephant.jpg'
+		},
+		{
+			name:'fox',
+			img: 'fox.jpg'
+		},
+		{
+			name:'monkey',
+			img: 'monkey.jpg'
+		},
+		{
+			name:'snake',
+			img: 'snake.jpg'
+		},
+		{
+			name:'tiger',
+			img: 'tiger.jpg'
+		},
+		{
+			name:'dog',
+			img: 'dog.jpg'
+		},
+		{
+			name:'monkey',
+			img: 'monkey.jpg'
+		},
+		{
+			name:'fox',
+			img: 'fox.jpg'
+		},
+		{
+			name:'snake',
+			img: 'snake.jpg'
+		},
+		{
+			name:'tiger',
+			img: 'tiger.jpg'
+		},
+		{
+			name:'elephant',
+			img: 'elephant.jpg'
+		},
+	]
+
+	cardArray1.sort(() => 0.5 - Math.random());
+
+	const grid1 = document.querySelector('.grid');
+	const resultDisplay1 = document.querySelector('#result');
+	var cardsChoosen1  = [];
+	var cardsChoosenId1 = [];
+	var cardsWon1 = [];
+
+	var card_image1= document.createElement('img');
+	card_image1.setAttribute('src', 'blank.jpg');
+
+	function createBoard1() {
+		for (let i = 0; i < cardArray1.length; i++) {
+			var card1 = document.createElement('img');
+			card1.setAttribute('src', 'blank.jpg');
+			card1.setAttribute('data-id', i);
+			card1.style.margin = "20px";
+			card1.addEventListener('click', flipCard1);
+			grid1.appendChild(card1);
+		}
+	}
+
+	function checkForMatch1() {
+		var cards1 = document.querySelectorAll('img');
+		const optionOneId1 = cardsChoosenId1[0];
+		const optionTwoId1 = cardsChoosenId1[1];
+		if(cardsChoosen1[0] === cardsChoosen1[1]) {
+			cards1[optionOneId1].setAttribute('src', 'white.jpg');
+			cards1[optionTwoId1].setAttribute('src', 'white.jpg');
+			cardsWon1.push(cardsChoosen1);
+			cards1[optionOneId1].setAttribute("disabled","disabled");
+			cards1[optionTwoId1].setAttribute("disabled","disabled");
+		} else {
+			cards1[optionOneId1].setAttribute('src', 'blank.jpg');
+			cards1[optionTwoId1].setAttribute('src', 'blank.jpg');
+		}
+		cardsChoosen1  = [];
+		cardsChoosenId1 = [];
+		resultDisplay1.textContent = cardsWon1.length;
+		if(cardsWon1.length === cardArray1.length/2) {
+			resultDisplay1.textContent = cardsWon1.length;
+			windows.location.href = 'level2.js';
+		}
+	}
+
+	function flipCard1() {
+		var cardId = this.getAttribute('data-id');
+		cardsChoosen1.push(cardArray1[cardId].name);
+		cardsChoosenId1.push(cardId);
+		this.setAttribute('src', cardArray1[cardId].img);
+		if(cardsChoosen1.length == 2) {
+			setTimeout(checkForMatch1, 500);
+		}
+	}
 
 		}
 	}
