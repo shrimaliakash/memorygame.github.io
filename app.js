1ae9a83881ca06e383cb0509566d98876347d868
Akash Shrimali <31008778+shrimaliakash@users.noreply.github.com>
2021-10-23 00:19:42 +0530

    make a responsive for all devices

--- app.js
+++ app.js
@@ -144,6 +144,38 @@ document.addEventListener('DOMContentLoaded', () => {
 			name:'banana',
 			img: 'banana.jpg'
 		},
+		{
+			name:'orange',
+			img: 'orange.jpg'
+		},
+		{
+			name:'grapes',
+			img: 'grapes.jpg'
+		},
+		{
+			name:'stobarry',
+			img: 'stobarry.jpg'
+		},
+		{
+			name:'watermelon',
+			img: 'watermelon.jpg'
+		},
+		{
+			name:'dog',
+			img: 'dog.jpg'
+		},
+		{
+			name:'elephant',
+			img: 'elephant.jpg'
+		},
+		{
+			name:'fox',
+			img: 'fox.jpg'
+		},
+		{
+			name:'monkey',
+			img: 'monkey.jpg'
+		}
 	]
 
 	cardArray.sort(() => 0.5 - Math.random());
